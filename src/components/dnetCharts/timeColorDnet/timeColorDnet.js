9458d44ce1daba0859b4eb59b18caf9214f6b0b4
Yichao <403624799@qq.com>
2020-12-15 18:26:49 +0800

    添加legend

--- src/components/dnetCharts/timeColorDnet/timeColorDnet.js
+++ src/components/dnetCharts/timeColorDnet/timeColorDnet.js
@@ -5,8 +5,9 @@ import { getPiePathColor } from '../../../util/dnetChart.js'
 
 export default function TimeColorDnet(props) {
     if (props.len === 0) return null
+    const legendData = new Array(props.len).fill(0)
+    const singleLegendWidth = 15
     let colorScale = getPiePathColor(props.len,'red', 'green')
-    // let colorScale = getPiePathColor(props.len)
     return (
         <div
             style={{
@@ -15,7 +16,48 @@ export default function TimeColorDnet(props) {
                 overflowX: 'auto'
             }}
             className="TimePositionDnet graph"
-        >
+        >   
+            <div
+                style={{
+                    width: '100%',
+                    height: '20px',
+                    display: 'flex',
+                    justifyContent: 'flex-end',
+                    margin: '5px'
+                }}
+            >
+                <div
+                    className="legend-text"
+                    style={{
+                        margin: '0 10px'
+                    }}
+                >0</div>
+                <svg
+                    className="legend-svg"
+                    width={`${singleLegendWidth*props.len}px`}
+                    height={'20px'}
+                    viewBox={`0 0 ${singleLegendWidth*props.len} 20`}
+                    preserveAspectRatio="xMinYMin"
+                >   
+                    {legendData.map((dataItem, index) => {
+                        return (
+                            <rect
+                                key={`tc-legend-${index}`}
+                                x={index*singleLegendWidth}
+                                y={0}
+                                fill={colorScale(index)}
+                                width={singleLegendWidth}
+                                height={singleLegendWidth}
+                            ></rect>
+                        )
+                    })}
+                </svg>
+                <div
+                   style={{
+                        margin: '0 10px'
+                    }}
+                >{props.len-1}</div>
+            </div>
             <svg
                 className="nlg-container-svg"
                 width={`${props.width}px`}
